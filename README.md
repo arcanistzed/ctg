0e5ad1c7c3065921cafcf52464c629ce521e41d9
arcanist <arcanistzed@gmail.com>
2021-08-30 14:53:39 -0400

    Initial Upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Combat Tracker Groups
+
+![Version](https://img.shields.io/github/v/tag/arcanistzed/ctg?label=Version&style=flat-square&color=2577a1) ![Latest Release Download Count](https://img.shields.io/github/downloads/arcanistzed/ctg/latest/module.zip?label=Downloads&style=flat-square&color=9b43a8) ![Foundry Core Compatible Version](https://img.shields.io/badge/dynamic/json.svg?url=https%3A%2F%2Fraw.githubusercontent.com%2Farcanistzed%2Fctg%2Fmain%2Fmodule.json&label=Foundry%20Core%20Compatible%20Version&query=$.compatibleCoreVersion&style=flat-square&color=ff6400)
+
+Group combatants with the same initiative under helpful toggles in the Combat Tracker.
+
+![ctg](CHANGE)
+
+## Installation
+
+In the setup screen, use the URL `https://github.com/arcanistzed/ctg/releases/latest/download/module.json` to install the module.
+
+## Usage
+
+## License
+
+Copyright © 2021 arcanist
+
+This package is under an [MIT license](LICENSE) and the [Foundry Virtual Tabletop Limited License Agreement for module development](https://foundryvtt.com/article/license/).
+
+See the [Notice](./NOTICE.md) for attribution details.
+
+## Bugs
+
+You can submit bugs via [Github Issues](https://github.com/arcanistzed/ctg/issues/new/choose) or on [my Discord server](https://discord.gg/AAkZWWqVav).
+
+## Contact me
+
+Come hang out on my [my Discord server](https://discord.gg/AAkZWWqVav) or [click here to send me an email](mailto:arcanistzed@gmail.com?subject=Initiative%20Toggles%20module).
---
+++ scripts/module.js
@@ -0,0 +1,149 @@
+export default class Ctg {
+    constructor() {
+        Hooks.on("init", () => {
+            game.settings.register(Ctg.ID, "mode", {
+                scope: "world",
+                config: false,
+                type: String,
+                default: "initiative",
+                onChange: mode => this.createGroups(mode)
+            });
+        });
+
+        Hooks.on("renderCombatTracker", (_app, html, options) => {
+            // Exit if there is no combat
+            if (!options.combat) return;
+
+            // Create modes if GM
+            if (game.user.isGM) this.createModes(html[0]);
+            // Create groups
+            this.createGroups(game.settings.get(Ctg.ID, "mode"))
+
+            // Add a listner to the mode container if GM
+            if (game.user.isGM) document.querySelector("#ctg-modeContainer").addEventListener('click', event => {
+                const mode = event.target.id?.replace("ctg-mode-radio-", "");
+                if (Ctg.MODES.includes(mode)) game.settings.set(Ctg.ID, "mode", mode);
+            });
+        });
+
+    };
+
+    /** The module's ID */
+    static ID = "ctg";
+
+    /** Grouping Modes */
+    static MODES = ["initiative", "name"];
+
+    /**
+     * Create Combat Tracker modes
+     * @param {HTMLElement} html - The Combat Tracker's HTML
+     */
+    createModes(html) {
+        /** Create container for mode selection boxes */
+        const container = document.createElement("ul"); container.id = "ctg-modeContainer";
+        html.querySelector("#combat > #combat-round").after(container);
+
+        // For each mode that exists
+        Ctg.MODES.forEach(m => {
+            // Add a box
+            const modeBox = document.createElement("li"); modeBox.id = "ctg-modeBox";
+            container.append(modeBox);
+
+            // Create a radio button
+            const radio = document.createElement("input"); radio.id = "ctg-mode-radio-" + m;
+            radio.type = "radio"; radio.name = "ctg-mode-radio";
+
+            // Create a label for the radio button
+            const label = document.createElement("label"); label.id = "ctg-modeLabel";
+            label.htmlFor = "ctg-mode-radio-" + m;
+            label.title = "Group by " + m.capitalize();
+            label.innerText = m.capitalize();
+
+            // Add the label and the radio button to the box
+            modeBox.append(radio);
+            modeBox.append(label);
+        });
+    };
+
+    /**
+     * Create Combat Tracker groups
+     * @param {String} mode - The mode that is currently enabled @see {@link modes}
+     */
+    createGroups(mode) {
+        // Remove any existing groups
+        document.querySelectorAll("details.ctg-toggle > li.combatant").forEach(combatant => document.querySelector("#combat-tracker").append(combatant));
+        document.querySelectorAll("details.ctg-toggle").forEach(toggle => toggle.remove());
+
+        // Show current mode if GM
+        if (game.user.isGM) document.querySelector("#ctg-mode-radio-" + mode).checked = true;
+
+        /** Group combatants into positions */
+        const groups = Object.values(game.combats.active.turns.reduce((accumulator, currentValue) => {
+            accumulator[currentValue[mode]] = [...accumulator[currentValue[mode]] || [], currentValue];
+            return accumulator;
+        }, {}));
+
+        // Create groups
+        // Go through each of the groups
+        groups.forEach(group => {
+            /** Toggle which contains combatants */
+            const toggle = document.createElement("details"); toggle.classList.add("ctg-toggle");
+            
+            /** Name of the current group */
+            let groupNames = [];
+
+            // Go through each of the combatants at this position   
+            group.forEach((combatant, i, arr) => {
+                /** The DOM element of this combatant */
+                const element = document.querySelector(`[data-combatant-id="${combatant.id}"]`);
+
+                // Add the name of the current combatant to the group names if it's not already there
+                if (!groupNames.includes(combatant.name)) groupNames.push(combatant.name);
+
+                // If it's the first entry
+                if (i === arr.length - 1) {
+                    // Add the toggle here
+                    element.before(toggle);
+
+                    // Create a label for the toggle
+                    const labelBox = document.createElement("summary"); labelBox.classList.add("ctg-labelBox");
+                    const labelFlex = document.createElement("div"); labelFlex.classList.add("ctg-labelFlex");
+                    const labelName = document.createElement("div"); labelName.classList.add("ctg-labelName");
+                    const labelCount = document.createElement("div"); labelCount.classList.add("ctg-labelCount");
+                    const labelValue = document.createElement("div"); labelValue.classList.add("ctg-labelValue");
+
+                    // Add the group name to the label
+                    labelName.innerText = groupNames.length < 3 ? groupNames.join(" and ") : groupNames.join(", ");
+                    // Add the value to the label if not in name mode
+                    if (mode !== "name") labelValue.innerText = combatant[mode];
+                    // Add the count to the label
+                    labelCount.innerText = arr.length;
+
+                    // Insert the label box
+                    toggle.prepend(labelBox);
+                    // Insert the label flex into the label box
+                    labelBox.prepend(labelFlex);
+                    // Insert the name into the label flex
+                    labelFlex.prepend(labelName);
+                    // Insert the count into the label flex
+                    labelFlex.append(labelCount);
+                    // Insert the value into the label flex
+                    labelFlex.append(labelValue);
+
+                };
+
+                // Move the element into the toggle
+                toggle.append(element);
+            });
+        });
+
+        // Get the current toggle
+        const currentToggle = document.querySelector(`[data-combatant-id="${game.combat.current.combatantId}"]`).parentElement
+        // Open the toggle for the current combatant
+        currentToggle.open = true;
+        // Darken the current toggle's label box
+        currentToggle.querySelector(".ctg-labelBox").style.filter = "brightness(0.5)";
+    };
+};
+
+new Ctg();
