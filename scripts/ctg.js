5c0eb156526e6e047d37dfcac9bb2f7cc4c798a6
arcanist <arcanistzed@gmail.com>
2021-11-11 22:32:06 -0500

    fix: tempfix for autosave bug
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- scripts/ctg.js
+++ scripts/ctg.js
@@ -74,8 +74,8 @@ export default class Ctg {
                 if (game.user.isGM) this.rollGroupInitiative();
             });
 
-            // Re-render Combat Tracker when mobs update
-            Hooks.on("matMobUpdate", () => ui.combat.render(true));
+            // Re-render Combat Tracker when mobs update not from autosave (FIXME: a re-render is needed, but is not being included to avoid a MAT bug. See https://github.com/Stendarpaval/mob-attack-tool/issues/46)
+            if (!game.settings.get("mob-attack-tool", "autoSaveCTGgroups")) Hooks.on("matMobUpdate", () => ui.combat.render(true));
 
             // Run group skipping code
             this.groupSkipping();
