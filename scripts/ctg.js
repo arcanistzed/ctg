e9e4acf941da324488ab43678a018f39970d9074
arcanist <arcanistzed@gmail.com>
2021-11-15 01:30:30 -0500

    feat: separate name display into new method
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- scripts/ctg.js
+++ scripts/ctg.js
@@ -179,28 +179,10 @@ export default class Ctg {
                 const subdirectory = document.createElement("ol"); subdirectory.classList.add("subdirectory");
                 toggle.append(subdirectory);
 
-                /** Names in the current group */
-                let names = [];
-
                 // Go through each of the combatants
                 group.forEach((combatant, i, arr) => {
                     /** The DOM element of this combatant */
                     const element = html.querySelector(`[data-combatant-id="${combatant.id}"]`);
-
-                    /** The name of this combatant in the Tracker */
-                    const trackerName = element.querySelector(".token-name > h4").textContent;
-
-                    // Add the name of the current combatant if it is not already
-                    if (!names.includes(combatant.name)) names.push((
-                        // Compatibility with CUB Hide Actor Names: check whether the name is being hidden by CUB
-                        game.modules.get("combat-utility-belt")?.active && game.settings.get("combat-utility-belt", "enableHideNPCNames")
-                        && (
-                            (game.settings.get("combat-utility-belt", "enableHideHostileNames") && trackerName === game.settings.get("combat-utility-belt", "hostileNameReplacement"))
-                            || (game.settings.get("combat-utility-belt", "enableHideNeutralNames") && trackerName === game.settings.get("combat-utility-belt", "neutralNameReplacement"))
-                            || (game.settings.get("combat-utility-belt", "enableHideFriendlyNames") && trackerName === game.settings.get("combat-utility-belt", "friendlyNameReplacement"))
-                        )
-                        // Add the name in the tracker instead if it has been hidden
-                    ) ? trackerName : combatant.name);
 
                     // If it's the last entry
                     if (i === arr.length - 1) {
@@ -215,7 +197,8 @@ export default class Ctg {
                         const labelValue = document.createElement("div"); labelValue.classList.add("ctg-labelValue");
 
                         // Add the group name to the label
-                        labelName.innerText = names.length < 3 ? names.join(" and ") : names.join(", ");
+                        labelName.innerText = Ctg.getDisplayName(group);
+
                         // Add the value to the label if not in name mode
                         if (mode === "initiative") labelValue.innerText = getProperty(combatant, Ctg.MODES.find(m => m[0] === mode).slice(-1)[0]);
                         // Add the count to the label
@@ -264,6 +247,41 @@ export default class Ctg {
             };
         };
     };
+
+    /** Get display name of a given group
+     * @static
+     * @param {Array<Combatant>} group - The group for which to return a name
+     * @return {String} Concatenated display name for this group 
+     * @memberof Ctg
+     */
+    static getDisplayName(group) {
+        /** Names in the current group */
+        let names = [];
+
+        // Go through all of the combatants in the group
+        group.forEach(combatant => {
+            /** The DOM element of this combatant */
+            const element = ui.combat.element[0].querySelector(`[data-combatant-id="${combatant?.id}"]`);
+
+            /** The name of this combatant in the Tracker */
+            const trackerName = element?.querySelector(".token-name > h4").textContent;
+
+            // Add the name of the current combatant if it is not already
+            if (!names.includes(combatant?.name)) names.push((
+                // Compatibility with CUB Hide Actor Names: check whether the name is being hidden by CUB
+                game.modules.get("combat-utility-belt")?.active && game.settings.get("combat-utility-belt", "enableHideNPCNames")
+                && (
+                    (game.settings.get("combat-utility-belt", "enableHideHostileNames") && trackerName === game.settings.get("combat-utility-belt", "hostileNameReplacement"))
+                    || (game.settings.get("combat-utility-belt", "enableHideNeutralNames") && trackerName === game.settings.get("combat-utility-belt", "neutralNameReplacement"))
+                    || (game.settings.get("combat-utility-belt", "enableHideFriendlyNames") && trackerName === game.settings.get("combat-utility-belt", "friendlyNameReplacement"))
+                )
+                // Add the name in the tracker instead if it has been hidden
+            ) ? trackerName : combatant?.name);
+        });
+
+        // Return a string with the names
+        return names.length < 3 ? names.join(" and ") : names.join(", ");
+    }
 
     /** Group Combatants
      * @static
